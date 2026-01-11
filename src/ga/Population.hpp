564e7895688285455af59533b2df159eba5525bb
Chopan22 <francisco.cardenas22.22@gmail.com>
2026-01-11 12:34:54 -0300

    feat: add population cpp hpp

---
+++ src/ga/Population.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <vector>
+#include <memory>
+#include "Chromosome.hpp"
+#include "Fitness.hpp"
+
+namespace ga {
+
+class Population {
+public:
+    Population(size_t populationSize,
+               Fitness& fitnessEvaluator,
+               double crossoverProb = 0.8,
+               double mutationProb = 0.2,
+               size_t tournamentSize = 3);
+
+    void initialize();
+    void evolve();
+
+    Chromosome getBest() const;
+    const std::vector<Chromosome>& getChromosomes() const { return chromosomes_; }
+
+private:
+    size_t populationSize_;
+    Fitness& fitness_;
+    double crossoverProb_;
+    double mutationProb_;
+    size_t tournamentSize_;
+
+    std::vector<Chromosome> chromosomes_;
+    std::vector<double> fitnessValues_;
+
+    void evaluateFitness();
+    Chromosome tournamentSelect();
+};
+}
